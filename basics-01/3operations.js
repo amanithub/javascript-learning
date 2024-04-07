fb95873b394cfc9c3f62e642fe2b5f59c4ae91ae
Aman Pandey <91895391+amanithub@users.noreply.github.com>
2024-04-07 14:21:39 +0000

    Updated datasummary

---
+++ basics-01/3operations.js
@@ -0,0 +1,7 @@
+console.log(1 <= "2")
+
+let name = 5
+
+let name1 = "Sohan"
+let name2 = false
+console.log(typeof name)
---
+++ basics-01/4_datatypes-summary.js
@@ -0,0 +1,42 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol(123)
+const anotherId = Symbol(123)
+
+// console.log(id === anotherId);
+// console.log(typeof anotherId)
+// console.log(typeof id)
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "batman"];
+let myObj = {
+    name: "amanpandey",
+    age: 22,
+}
+// console.table(heros)
+// console.log(myObj)
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+myFunction();
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3

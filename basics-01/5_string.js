8d21677b418154c5975e874dc898846840d6aa8d
Aman Pandey <91895391+amanithub@users.noreply.github.com>
2024-04-10 10:32:28 +0000

    studied string and maths

---
+++ basics-01/5_string.js
@@ -0,0 +1,21 @@
+let name = "Amanum"
+let age = 22
+
+
+console.log(`My name is ${name} and I'm a good javascript developer of Age: ${age}`)
+
+let strname = new String("jitGub-Github-git-youtube");
+
+// console.log(strname.length);
+// console.log(strname.toUpperCase());
+// console.log(strname.toLocaleLowerCase());
+// console.log(strname.charAt(5));
+// console.log(strname.indexOf('G'));
+
+const newString = strname.slice(-7, 6)
+
+// console.log(newString);
+
+// console.log(strname.split("i"));
+
+
---
+++ basics-01/6_maths-and-numbers.js
@@ -0,0 +1,11 @@
+// console.log(Math.round(4.4));
+
+// console.log(Math.pow(2,4));
+
+const dice = Math.random()*6 + 1
+
+console.log(dice.toPrecision(1));
+
+const url = "https://supreme-parakeet-pqqq96rjw6426x6p.github.dev/"
+
+console.log(url.includes('pqqq9'));

1cd3394fcd515ae041189a4f1a31f5300a7260ba
Aman Pandey <91895391+amanithub@users.noreply.github.com>
2024-04-12 04:14:43 +0000

    arrays

--- .devcontainer/basics-02/01_array.js
+++ .devcontainer/basics-02/01_array.js
@@ -1,3 +1,16 @@
 const myArr = [99, 98, 97, 96, 93];
 
-console.log(myArr[3]);
+// console.log(myArr[3]);
+
+// myArr.push(0, 6);
+// myArr.unshift(0)
+// console.log(myArr);
+
+const newArray = myArr.slice(1,3);
+
+console.log(newArray);
+console.log("A:", myArr);
+
+const newspArray = myArr.splice(1,3);
+console.log(newspArray);
+console.log("B:", myArr);
---
+++ .devcontainer/basics-02/2_arrays.js
@@ -0,0 +1,11 @@
+const firstname = ["Aman, Rahul, Mayank, Abhishek"];
+
+const surname = ["Pandey, Singh, Prasad, Narine"];
+const college = ["NIEC, MAIT, SSBUICET, PEC"];
+const fullname = firstname.concat(surname).concat(college);
+
+console.log(fullname);
+
+const fullname2 = [...firstname, ...surname, ...college];
+
+// console.log(fullname2)
